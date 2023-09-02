0ed8ce9cc3b0257452736ee635fb1882c36b3a66
WuXingYu <1043397668@qq.com>
2023-09-02 13:03:17 +0800

    '验证'

--- SingletonPattern/src/Main.java
+++ SingletonPattern/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        System.out.println(instance1 == instance2);  // true
     }
 }
